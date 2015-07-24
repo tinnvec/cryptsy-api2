e3fa9b53fa5b89b4e8b1e518adef4da3e66ca0a3
Alex Hanna <tinnvec@gmail.com>
2015-07-23 20:31:18 -0500

    Add Converter Class

---
+++ lib/cryptsy/api2/converter.rb
@@ -0,0 +1,23 @@
+module Cryptsy
+  module API2
+    class Converter
+
+      def initialize(public_key=nil, private_key=nil)
+        @public_key = public_key
+        @private_key = private_key
+      end
+
+      def create(options)
+        Request.send("converter", options, @public_key, @private_key, "POST")
+      end
+
+      def info(quote_id)
+        Request.send("converter/#{quote_id}", {}, @public_key, @private_key)
+      end
+
+      def depositaddress(quote_id)
+        Request.send("converter/#{quote_id}/depositaddress", {}, @public_key, @private_key)
+      end
+    end
+  end
+end
