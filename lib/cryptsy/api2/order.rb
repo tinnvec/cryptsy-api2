2d8bf46f08fec88d8a28d87ca36000fcc1aa0cf6
Alex Hanna <tinnvec@gmail.com>
2015-07-23 20:25:20 -0500

    Add Order Class (+1 squashed commits)
    Squashed commits:
    
    [8f87694] Update Order class

---
+++ lib/cryptsy/api2/order.rb
@@ -0,0 +1,23 @@
+module Cryptsy
+  module API2
+    class Currencies
+
+      def initialize(public_key=nil, private_key=nil)
+        @public_key = public_key
+        @private_key = private_key
+      end
+
+      def create(options)
+        Request.send("order", options, @public_key, @private_key, "POST")
+      end
+
+      def info(order_id)
+        Request.send("order/#{order_id}", {}, @public_key, @private_key)
+      end
+
+      def delete(order_id)
+        Request.send("order/#{order_id}", {}, @public_key, @private_key, "DELETE")
+      end
+    end
+  end
+end
