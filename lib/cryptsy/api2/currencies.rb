856167daa03632febd15c0371f0bd0bb607a5eb8
Alex Hanna <tinnvec@gmail.com>
2015-07-23 20:20:09 -0500

    Add Currencies class

---
+++ lib/cryptsy/api2/currencies.rb
@@ -0,0 +1,22 @@
+module Cryptsy
+  module API2
+    class Currencies
+
+      def initialize(public_key=nil, private_key=nil)
+        @public_key = public_key
+        @private_key = private_key
+      end
+
+      def list
+        Request.send("currencies")
+      end
+
+      def info(currency_id)
+        Request.send("currencies/#{currency_id}")
+      end
+
+      # Need extra special access for these, think about testing later?
+      #
+      # def status(currency_id)
+      #   Request.send("currencies/#{currency_id}/status", {}, @public_key, @private_key)
+      # end
