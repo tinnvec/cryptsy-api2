6e141d22874589799b7a5471e75c063e4fcb9f14
Alex Hanna <tinnvec@gmail.com>
2015-07-23 20:10:18 -0500

    Add Client and Request classes

---
+++ lib/cryptsy/api2.rb
@@ -0,0 +1,55 @@
+require 'uri'
+require 'net/http'
+require 'openssl'
+require 'json'
+
+require_relative 'api2/user'
+require_relative 'api2/markets'
+
+module Cryptsy
+  module API2
+    class Client
+      attr_accessor :user, :markets
+
+      def initialize(public_key=nil, private_key=nil)
+        @user = User.new(public_key, private_key)
+        @markets = Markets.new(public_key, private_key)
+      end
+    end
+
+    class Request
+
+      def self.send(path, query={}, public_key=nil, private_key=nil, method="GET")
+        auth = !public_key.nil? && !private_key.nil?
+        query[:nonce] = nonce if auth
+        uri = URI("https://api.cryptsy.com/api/v2/#{path}")
+        uri.query = URI.encode_www_form(query) unless query.empty?
+        Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https', ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
+          request = Net::HTTP::Get.new(uri)
+          request = Net::HTTP::Post.new(uri) if method == "POST"
+          request['Sign'] = sign(query, private_key) if auth
+          request['Key'] = public_key if auth
+          response = http.request(request)
+          output = JSON.parse(response.body)
+          return output['data'] if output['success']
+          return output
+        end
+      end
+
+      private
+
+        def self.nonce
+          (Time.now.to_f * 1000).round
+        end
+
+        def self.sign(query, private_key=nil)
+          OpenSSL::HMAC.hexdigest(
+            OpenSSL::Digest.new('sha512'),
+            private_key.encode('utf-8'),
+            URI.encode( query.map{ |k,v| "#{k}=#{v}" }.join("&") )
+          )
+        end
+    end
+
+  end
+end
