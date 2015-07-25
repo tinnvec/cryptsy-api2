730473e039d8369ed8517fda4ef4085dcc2b01a4
Alex Hanna <tinnvec@gmail.com>
2015-07-25 13:58:45 -0500

    Test updates

--- test/api2_test.rb
+++ test/api2_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'cryptsy/api2'
 
+ENV['CODECLIMATE_REPO_TOKEN'] = '6923f8340c8b1bc2fed3707c98224bd79c3e9836e082e27f4dd2aea5873b3182'
 require_relative 'test_helper'
 
 class TestAPI2 < Minitest::Test
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,5 @@
 require 'codeclimate-test-reporter'
-ENV['CODECLIMATE_REPO_TOKEN'] = '6923f8340c8b1bc2fed3707c98224bd79c3e9836e082e27f4dd2aea5873b3182'
-CodeClimate::TestReporter.configure do |config|
-  config.path_prefix = 'lib/cryptsy'
-end
+# CodeClimate::TestReporter.configure do |config|
+#   config.path_prefix = 'lib/cryptsy'
+# end
 CodeClimate::TestReporter.start
