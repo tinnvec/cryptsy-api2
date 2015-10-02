05bf26aeeac270b25a036b2424ce815809fc540e
Alex Hanna <tinnvec@gmail.com>
2015-10-02 16:52:40 -0400

    Add Client class attribute tests

--- spec/api2_spec.rb
+++ spec/api2_spec.rb
@@ -1,5 +1,13 @@
 require 'cryptsy/api2'
 
-describe Cryptsy::API2 do
-  let(:api2) { Class.new include API2 }
+describe Cryptsy::API2::Client do
+  subject { Cryptsy::API2::Client.new }
+  describe "attributes" do
+    it { is_expected.to respond_to(:user) }
+    it { is_expected.to respond_to(:markets) }
+    it { is_expected.to respond_to(:currencies) }
+    it { is_expected.to respond_to(:order) }
+    it { is_expected.to respond_to(:converter) }
+    it { is_expected.to respond_to(:trigger) }
+  end
 end
