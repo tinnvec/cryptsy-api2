104e94a3f28720fb63f3bbf272ed72d117f43bf0
Alex Hanna <tinnvec@gmail.com>
2015-10-08 17:21:29 -0400

    Start of Markets class tests

--- spec/api2_spec.rb
+++ spec/api2_spec.rb
@@ -17,17 +17,17 @@ describe Cryptsy::API2::Currencies do
   subject { Cryptsy::API2::Currencies.new }
 
   describe '#list' do
-    it 'returns non-empty JSON result' do
-      expect(subject.list.count).not_to eq 0
+    it 'returns more than one currency' do
+      expect(subject.list.count).to be > 1
     end
   end
 
   describe '#info' do
     context 'with currency_id of "BTC"' do
-      it 'returns info with correct name' do
+      it 'returns name info of "BitCoin"' do
         expect(subject.info('BTC')['name']).to eql "BitCoin"
       end
-      it 'returns info with correct id' do
+      it 'returns id info of "3"' do
         expect(subject.info('BTC')['id']).to eql "3"
       end
     end
@@ -35,12 +35,22 @@ describe Cryptsy::API2::Currencies do
 
   describe '#marketlist' do
     context 'with currency_id of "BTC"' do
-      it 'returns info with correct id' do
+      it 'returns id info of "3"' do
         expect(subject.marketlist('BTC')['id']).to eql "3"
       end
       it 'returns markets info' do
         expect(subject.marketlist('BTC')['markets']).not_to eql nil
       end
+    end
+  end
+end
+
+describe Cryptsy::API2::Markets do
+  subject { Cryptsy::API2::Markets.new }
+
+  describe '#list' do
+    it 'returns more than one market' do
+      expect(subject.list.count).to be > 1
     end
   end
 end
